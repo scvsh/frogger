b06e26427262e6f18dcf2e637ed629fc1640c6b4
Arseni Zemskow <scvsh@FleetwoodMac.local>
2017-11-05 20:35:12 +0300

    Add player class and move handler

--- js/app.js
+++ js/app.js
@@ -1,31 +1,84 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+let numRows = 6;
+let numCols = 5;
 
+class Enemy {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    constructor() {
+        this.sprite = 'images/enemy-bug.png';
+        this.x = 0;
+    }
+
+    update(dt) {
+        this.x += dt*2;
+
+    }
+    //
+    // Draw the enemy on the screen, required method for game
+    // 
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// You should multiply any movement by the dt parameter
+// which will ensure the game runs at the same speed for
+// all computers.
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor() {
+        this.sprite = 'images/char-boy.png';
+        this.x = numCols / 2 * 83; //Engine.numCols/2 * 83; 
+        this.y = (numRows - 2) * 101; //Engine.numCols/2 * 83;
+    }
 
+    update() {
+        return true;
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 
+    handleInput(e) {
+        switch (e) {
+            case 'left':
+                if (this.x >= 101) {
+                    this.x -= 101
+                }
+                break;
+
+            case 'up':
+                if (this.y >= 0) {
+                    this.y -= 83;
+                }
+                break;
+
+            case 'right':
+                if (this.x <= 404) {
+                    this.x += 101;
+                }
+                break;
+
+            case 'down':
+                if (this.y <= 321) {
+                    this.y += 83;
+                }
+                break;
+        }
+    }
+}
+
+let allEnemies = [];
+let player = new Player();
+for (let i = 0; i < 10; i++) {
+    allEnemies.push(new Enemy());
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
